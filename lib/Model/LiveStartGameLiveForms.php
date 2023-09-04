3f4181a80b47ed6ac370627fd80112f5ca9d3c87
Iven <huanghanwen.hhw@alibaba-inc.com>
2023-09-04 10:42:09 +0800

    feat: update

---
+++ lib/Model/LiveStartGameLiveForms.php
@@ -0,0 +1,377 @@
+<?php
+/**
+ * LiveStartGameLiveForms
+ *
+ * PHP version 5
+ *
+ * @category Class
+ * @package  Yjopenapi\Client
+ */
+
+/** Package client
+ * 
+ * 
+ * YuanJing OpenAPI SDK for PHP
+ *
+ * 
+ *
+ */
+
+namespace Yjopenapi\Client\Model;
+
+use \ArrayAccess;
+use \Yjopenapi\Client\Api\ObjectSerializer;
+
+/**
+ * LiveStartGameLiveForms Class Doc Comment
+ *
+ * @category Class
+ * @package  Yjopenapi\Client
+ */
+class LiveStartGameLiveForms implements ModelInterface, ArrayAccess
+{
+    const DISCRIMINATOR = null;
+
+    /**
+      * The original name of the model.
+      *
+      * @var string
+      */
+    protected static $serialModelName = 'LiveStartGameLiveForms';
+
+    /**
+      * Array of property to type mappings. Used for (de)serialization
+      *
+      * @var string[]
+      */
+    protected static $serialTypes = [
+        'app_key' => 'string',
+'game_session' => 'string',
+'server_url' => 'string',
+'stream_key' => 'string',
+'config' => '\Yjopenapi\Client\Model\LiveStartGameLiveFormsConfig'    ];
+
+    /**
+      * Array of property to format mappings. Used for (de)serialization
+      *
+      * @var string[]
+      */
+    protected static $serialFormats = [
+        'app_key' => null,
+'game_session' => null,
+'server_url' => null,
+'stream_key' => null,
+'config' => null    ];
+
+    /**
+     * Array of property to type mappings. Used for (de)serialization
+     *
+     * @return array
+     */
+    public static function serialTypes()
+    {
+        return self::$serialTypes;
+    }
+
+    /**
+     * Array of property to format mappings. Used for (de)serialization
+     *
+     * @return array
+     */
+    public static function serialFormats()
+    {
+        return self::$serialFormats;
+    }
+
+    /**
+     * Array of attributes where the key is the local name,
+     * and the value is the original name
+     *
+     * @var string[]
+     */
+    protected static $attributeMap = [
+        'app_key' => 'appKey',
+'game_session' => 'gameSession',
+'server_url' => 'serverUrl',
+'stream_key' => 'streamKey',
+'config' => 'config'    ];
+
+    /**
+     * Array of attributes to setter functions (for deserialization of responses)
+     *
+     * @var string[]
+     */
+    protected static $setters = [
+        'app_key' => 'setAppKey',
+'game_session' => 'setGameSession',
+'server_url' => 'setServerUrl',
+'stream_key' => 'setStreamKey',
+'config' => 'setConfig'    ];
+
+    /**
+     * Array of attributes to getter functions (for serialization of requests)
+     *
+     * @var string[]
+     */
+    protected static $getters = [
+        'app_key' => 'getAppKey',
+'game_session' => 'getGameSession',
+'server_url' => 'getServerUrl',
+'stream_key' => 'getStreamKey',
+'config' => 'getConfig'    ];
+
+    /**
+     * Array of attributes where the key is the local name,
+     * and the value is the original name
+     *
+     * @return array
+     */
+    public static function attributeMap()
+    {
+        return self::$attributeMap;
+    }
+
+    /**
+     * Array of attributes to setter functions (for deserialization of responses)
+     *
+     * @return array
+     */
+    public static function setters()
+    {
+        return self::$setters;
+    }
+
+    /**
+     * Array of attributes to getter functions (for serialization of requests)
+     *
+     * @return array
+     */
+    public static function getters()
+    {
+        return self::$getters;
+    }
+
+    /**
+     * The original name of the model.
+     *
+     * @return string
+     */
+    public function getModelName()
+    {
+        return self::$serialModelName;
+    }
+
+    
+
+    /**
+     * Associative array for storing property values
+     *
+     * @var mixed[]
+     */
+    protected $container = [];
+
+    /**
+     * Constructor
+     *
+     * @param mixed[] $data Associated array of property values
+     *                      initializing the model
+     */
+    public function __construct(array $data = null)
+    {
+        $this->container['app_key'] = isset($data['app_key']) ? $data['app_key'] : null;
+        $this->container['game_session'] = isset($data['game_session']) ? $data['game_session'] : null;
+        $this->container['server_url'] = isset($data['server_url']) ? $data['server_url'] : null;
+        $this->container['stream_key'] = isset($data['stream_key']) ? $data['stream_key'] : null;
+        $this->container['config'] = isset($data['config']) ? $data['config'] : null;
+    }
+
+
+    /**
+     * Gets app_key
+     *
+     * @return string
+     */
+    public function getAppKey()
+    {
+        return $this->container['app_key'];
+    }
+
+    /**
+     * Sets app_key
+     *
+     * @param string $app_key app_key
+     *
+     * @return $this
+     */
+    public function setAppKey($app_key)
+    {
+        $this->container['app_key'] = $app_key;
+
+        return $this;
+    }
+
+    /**
+     * Gets game_session
+     *
+     * @return string
+     */
+    public function getGameSession()
+    {
+        return $this->container['game_session'];
+    }
+
+    /**
+     * Sets game_session
+     *
+     * @param string $game_session game_session
+     *
+     * @return $this
+     */
+    public function setGameSession($game_session)
+    {
+        $this->container['game_session'] = $game_session;
+
+        return $this;
+    }
+
+    /**
+     * Gets server_url
+     *
+     * @return string
+     */
+    public function getServerUrl()
+    {
+        return $this->container['server_url'];
+    }
+
+    /**
+     * Sets server_url
+     *
+     * @param string $server_url server_url
+     *
+     * @return $this
+     */
+    public function setServerUrl($server_url)
+    {
+        $this->container['server_url'] = $server_url;
+
+        return $this;
+    }
+
+    /**
+     * Gets stream_key
+     *
+     * @return string
+     */
+    public function getStreamKey()
+    {
+        return $this->container['stream_key'];
+    }
+
+    /**
+     * Sets stream_key
+     *
+     * @param string $stream_key stream_key
+     *
+     * @return $this
+     */
+    public function setStreamKey($stream_key)
+    {
+        $this->container['stream_key'] = $stream_key;
+
+        return $this;
+    }
+
+    /**
+     * Gets config
+     *
+     * @return \Yjopenapi\Client\Model\LiveStartGameLiveFormsConfig
+     */
+    public function getConfig()
+    {
+        return $this->container['config'];
+    }
+
+    /**
+     * Sets config
+     *
+     * @param \Yjopenapi\Client\Model\LiveStartGameLiveFormsConfig $config config
+     *
+     * @return $this
+     */
+    public function setConfig($config)
+    {
+        $this->container['config'] = $config;
+
+        return $this;
+    }
+    /**
+     * Returns true if offset exists. False otherwise.
+     *
+     * @param integer $offset Offset
+     *
+     * @return boolean
+     */
+    public function offsetExists($offset)
+    {
+        return isset($this->container[$offset]);
+    }
+
+    /**
+     * Gets offset.
+     *
+     * @param integer $offset Offset
+     *
+     * @return mixed
+     */
+    public function offsetGet($offset)
+    {
+        return isset($this->container[$offset]) ? $this->container[$offset] : null;
+    }
+
+    /**
+     * Sets value based on offset.
+     *
+     * @param integer $offset Offset
+     * @param mixed   $value  Value to be set
+     *
+     * @return void
+     */
+    public function offsetSet($offset, $value)
+    {
+        if (is_null($offset)) {
+            $this->container[] = $value;
+        } else {
+            $this->container[$offset] = $value;
+        }
+    }
+
+    /**
+     * Unsets offset.
+     *
+     * @param integer $offset Offset
+     *
+     * @return void
+     */
+    public function offsetUnset($offset)
+    {
+        unset($this->container[$offset]);
+    }
+
+    /**
+     * Gets the string presentation of the object
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        //if (defined('JSON_PRETTY_PRINT')) { // use JSON pretty print
+        //    return json_encode(
+        //        ObjectSerializer::sanitizeForSerialization($this),
+        //        JSON_PRETTY_PRINT
+        //    );
+        //}
+
+        return json_encode(ObjectSerializer::sanitizeForSerialization($this));
+    }
+}
