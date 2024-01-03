373cca320b53cb215762705d2f9fcac742c46b12
Iven <huanghanwen.hhw@alibaba-inc.com>
2024-01-04 00:31:08 +0800

    feat: update

---
+++ lib/Model/StopGameForms.php
@@ -0,0 +1,377 @@
+<?php
+/**
+ * StopGameForms
+ *
+ * PHP version 5
+ *
+ * @category Class
+ * @package  Yjopenapi\Client
+ */
+
+/** Package client
+ * 
+ * 
+ * YuanJing OpenAPI SDK for PHP
+ *
+ * 
+ *
+ */
+
+namespace Yjopenapi\Client\Yjopenapi\Client\Yjopenapi\Client\Yjopenapi\Client\Model;
+
+use \ArrayAccess;
+use \Yjopenapi\Client\Yjopenapi\Client\Yjopenapi\Client\Yjopenapi\Client\Api\ObjectSerializer;
+
+/**
+ * StopGameForms Class Doc Comment
+ *
+ * @category Class
+ * @package  Yjopenapi\Client
+ */
+class StopGameForms implements ModelInterface, ArrayAccess
+{
+    const DISCRIMINATOR = null;
+
+    /**
+      * The original name of the model.
+      *
+      * @var string
+      */
+    protected static $serialModelName = 'StopGameForms';
+
+    /**
+      * Array of property to type mappings. Used for (de)serialization
+      *
+      * @var string[]
+      */
+    protected static $serialTypes = [
+        'account_id' => 'string',
+'game_id' => 'string',
+'app_key' => 'string',
+'game_session' => 'string',
+'reason' => 'string'    ];
+
+    /**
+      * Array of property to format mappings. Used for (de)serialization
+      *
+      * @var string[]
+      */
+    protected static $serialFormats = [
+        'account_id' => null,
+'game_id' => null,
+'app_key' => null,
+'game_session' => null,
+'reason' => null    ];
+
+    /**
+     * Array of property to type mappings. Used for (de)serialization
+     *
+     * @return array
+     */
+    public static function serialTypes()
+    {
+        return self::$serialTypes;
+    }
+
+    /**
+     * Array of property to format mappings. Used for (de)serialization
+     *
+     * @return array
+     */
+    public static function serialFormats()
+    {
+        return self::$serialFormats;
+    }
+
+    /**
+     * Array of attributes where the key is the local name,
+     * and the value is the original name
+     *
+     * @var string[]
+     */
+    protected static $attributeMap = [
+        'account_id' => 'accountId',
+'game_id' => 'gameId',
+'app_key' => 'appKey',
+'game_session' => 'gameSession',
+'reason' => 'reason'    ];
+
+    /**
+     * Array of attributes to setter functions (for deserialization of responses)
+     *
+     * @var string[]
+     */
+    protected static $setters = [
+        'account_id' => 'setAccountId',
+'game_id' => 'setGameId',
+'app_key' => 'setAppKey',
+'game_session' => 'setGameSession',
+'reason' => 'setReason'    ];
+
+    /**
+     * Array of attributes to getter functions (for serialization of requests)
+     *
+     * @var string[]
+     */
+    protected static $getters = [
+        'account_id' => 'getAccountId',
+'game_id' => 'getGameId',
+'app_key' => 'getAppKey',
+'game_session' => 'getGameSession',
+'reason' => 'getReason'    ];
+
+    /**
+     * Array of attributes where the key is the local name,
+     * and the value is the original name
+     *
+     * @return array
+     */
+    public static function attributeMap()
+    {
+        return self::$attributeMap;
+    }
+
+    /**
+     * Array of attributes to setter functions (for deserialization of responses)
+     *
+     * @return array
+     */
+    public static function setters()
+    {
+        return self::$setters;
+    }
+
+    /**
+     * Array of attributes to getter functions (for serialization of requests)
+     *
+     * @return array
+     */
+    public static function getters()
+    {
+        return self::$getters;
+    }
+
+    /**
+     * The original name of the model.
+     *
+     * @return string
+     */
+    public function getModelName()
+    {
+        return self::$serialModelName;
+    }
+
+    
+
+    /**
+     * Associative array for storing property values
+     *
+     * @var mixed[]
+     */
+    protected $container = [];
+
+    /**
+     * Constructor
+     *
+     * @param mixed[] $data Associated array of property values
+     *                      initializing the model
+     */
+    public function __construct(array $data = null)
+    {
+        $this->container['account_id'] = isset($data['account_id']) ? $data['account_id'] : null;
+        $this->container['game_id'] = isset($data['game_id']) ? $data['game_id'] : null;
+        $this->container['app_key'] = isset($data['app_key']) ? $data['app_key'] : null;
+        $this->container['game_session'] = isset($data['game_session']) ? $data['game_session'] : null;
+        $this->container['reason'] = isset($data['reason']) ? $data['reason'] : null;
+    }
+
+
+    /**
+     * Gets account_id
+     *
+     * @return string
+     */
+    public function getAccountId()
+    {
+        return $this->container['account_id'];
+    }
+
+    /**
+     * Sets account_id
+     *
+     * @param string $account_id 用户id，给到Paas平台和SDK，两者保持一致，全局唯一
+     *
+     * @return $this
+     */
+    public function setAccountId($account_id)
+    {
+        $this->container['account_id'] = $account_id;
+
+        return $this;
+    }
+
+    /**
+     * Gets game_id
+     *
+     * @return string
+     */
+    public function getGameId()
+    {
+        return $this->container['game_id'];
+    }
+
+    /**
+     * Sets game_id
+     *
+     * @param string $game_id Paas平台部署的游戏Id
+     *
+     * @return $this
+     */
+    public function setGameId($game_id)
+    {
+        $this->container['game_id'] = $game_id;
+
+        return $this;
+    }
+
+    /**
+     * Gets app_key
+     *
+     * @return string
+     */
+    public function getAppKey()
+    {
+        return $this->container['app_key'];
+    }
+
+    /**
+     * Sets app_key
+     *
+     * @param string $app_key Paas平台AK(应用的AK，非服务端AK)
+     *
+     * @return $this
+     */
+    public function setAppKey($app_key)
+    {
+        $this->container['app_key'] = $app_key;
+
+        return $this;
+    }
+
+    /**
+     * Gets game_session
+     *
+     * @return string
+     */
+    public function getGameSession()
+    {
+        return $this->container['game_session'];
+    }
+
+    /**
+     * Sets game_session
+     *
+     * @param string $game_session 调度成功返回的GameSession
+     *
+     * @return $this
+     */
+    public function setGameSession($game_session)
+    {
+        $this->container['game_session'] = $game_session;
+
+        return $this;
+    }
+
+    /**
+     * Gets reason
+     *
+     * @return string
+     */
+    public function getReason()
+    {
+        return $this->container['reason'];
+    }
+
+    /**
+     * Sets reason
+     *
+     * @param string $reason 端侧从Paas sdk获取的调度业务参数
+     *
+     * @return $this
+     */
+    public function setReason($reason)
+    {
+        $this->container['reason'] = $reason;
+
+        return $this;
+    }
+    /**
+     * Returns true if offset exists. False otherwise.
+     *
+     * @param integer $offset Offset
+     *
+     * @return boolean
+     */
+    public function offsetExists($offset)
+    {
+        return isset($this->container[$offset]);
+    }
+
+    /**
+     * Gets offset.
+     *
+     * @param integer $offset Offset
+     *
+     * @return mixed
+     */
+    public function offsetGet($offset)
+    {
+        return isset($this->container[$offset]) ? $this->container[$offset] : null;
+    }
+
+    /**
+     * Sets value based on offset.
+     *
+     * @param integer $offset Offset
+     * @param mixed   $value  Value to be set
+     *
+     * @return void
+     */
+    public function offsetSet($offset, $value)
+    {
+        if (is_null($offset)) {
+            $this->container[] = $value;
+        } else {
+            $this->container[$offset] = $value;
+        }
+    }
+
+    /**
+     * Unsets offset.
+     *
+     * @param integer $offset Offset
+     *
+     * @return void
+     */
+    public function offsetUnset($offset)
+    {
+        unset($this->container[$offset]);
+    }
+
+    /**
+     * Gets the string presentation of the object
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        //if (defined('JSON_PRETTY_PRINT')) { // use JSON pretty print
+        //    return json_encode(
+        //        ObjectSerializer::sanitizeForSerialization($this),
+        //        JSON_PRETTY_PRINT
+        //    );
+        //}
+
+        return json_encode(ObjectSerializer::sanitizeForSerialization($this));
+    }
+}
