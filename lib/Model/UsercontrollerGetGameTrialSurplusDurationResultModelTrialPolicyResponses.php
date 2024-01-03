e13f5cc825dcfed65885eaafa9523558b8da9e78
Iven <huanghanwen.hhw@alibaba-inc.com>
2024-01-04 00:32:03 +0800

    feat: update

---
+++ lib/Model/UsercontrollerGetGameTrialSurplusDurationResultModelTrialPolicyResponses.php
@@ -0,0 +1,287 @@
+<?php
+/**
+ * UsercontrollerGetGameTrialSurplusDurationResultModelTrialPolicyResponses
+ *
+ * PHP version 5
+ *
+ * @category Class
+ * @package  Yjopenapi\Client
+ */
+
+/** Package client
+ * 
+ * 
+ * YuanJing OpenAPI SDK for PHP
+ *
+ * 
+ *
+ */
+
+namespace Yjopenapi\Client\Yjopenapi\Client\Yjopenapi\Client\Yjopenapi\Client\Model;
+
+use \ArrayAccess;
+use \Yjopenapi\Client\Yjopenapi\Client\Yjopenapi\Client\Yjopenapi\Client\Api\ObjectSerializer;
+
+/**
+ * UsercontrollerGetGameTrialSurplusDurationResultModelTrialPolicyResponses Class Doc Comment
+ *
+ * @category Class
+ * @package  Yjopenapi\Client
+ */
+class UsercontrollerGetGameTrialSurplusDurationResultModelTrialPolicyResponses implements ModelInterface, ArrayAccess
+{
+    const DISCRIMINATOR = null;
+
+    /**
+      * The original name of the model.
+      *
+      * @var string
+      */
+    protected static $serialModelName = 'UsercontrollerGetGameTrialSurplusDurationResultModelTrialPolicyResponses';
+
+    /**
+      * Array of property to type mappings. Used for (de)serialization
+      *
+      * @var string[]
+      */
+    protected static $serialTypes = [
+        'policy_type' => 'string',
+'surplus_duration_in_second' => 'int'    ];
+
+    /**
+      * Array of property to format mappings. Used for (de)serialization
+      *
+      * @var string[]
+      */
+    protected static $serialFormats = [
+        'policy_type' => null,
+'surplus_duration_in_second' => 'int64'    ];
+
+    /**
+     * Array of property to type mappings. Used for (de)serialization
+     *
+     * @return array
+     */
+    public static function serialTypes()
+    {
+        return self::$serialTypes;
+    }
+
+    /**
+     * Array of property to format mappings. Used for (de)serialization
+     *
+     * @return array
+     */
+    public static function serialFormats()
+    {
+        return self::$serialFormats;
+    }
+
+    /**
+     * Array of attributes where the key is the local name,
+     * and the value is the original name
+     *
+     * @var string[]
+     */
+    protected static $attributeMap = [
+        'policy_type' => 'policyType',
+'surplus_duration_in_second' => 'surplusDurationInSecond'    ];
+
+    /**
+     * Array of attributes to setter functions (for deserialization of responses)
+     *
+     * @var string[]
+     */
+    protected static $setters = [
+        'policy_type' => 'setPolicyType',
+'surplus_duration_in_second' => 'setSurplusDurationInSecond'    ];
+
+    /**
+     * Array of attributes to getter functions (for serialization of requests)
+     *
+     * @var string[]
+     */
+    protected static $getters = [
+        'policy_type' => 'getPolicyType',
+'surplus_duration_in_second' => 'getSurplusDurationInSecond'    ];
+
+    /**
+     * Array of attributes where the key is the local name,
+     * and the value is the original name
+     *
+     * @return array
+     */
+    public static function attributeMap()
+    {
+        return self::$attributeMap;
+    }
+
+    /**
+     * Array of attributes to setter functions (for deserialization of responses)
+     *
+     * @return array
+     */
+    public static function setters()
+    {
+        return self::$setters;
+    }
+
+    /**
+     * Array of attributes to getter functions (for serialization of requests)
+     *
+     * @return array
+     */
+    public static function getters()
+    {
+        return self::$getters;
+    }
+
+    /**
+     * The original name of the model.
+     *
+     * @return string
+     */
+    public function getModelName()
+    {
+        return self::$serialModelName;
+    }
+
+    
+
+    /**
+     * Associative array for storing property values
+     *
+     * @var mixed[]
+     */
+    protected $container = [];
+
+    /**
+     * Constructor
+     *
+     * @param mixed[] $data Associated array of property values
+     *                      initializing the model
+     */
+    public function __construct(array $data = null)
+    {
+        $this->container['policy_type'] = isset($data['policy_type']) ? $data['policy_type'] : null;
+        $this->container['surplus_duration_in_second'] = isset($data['surplus_duration_in_second']) ? $data['surplus_duration_in_second'] : null;
+    }
+
+
+    /**
+     * Gets policy_type
+     *
+     * @return string
+     */
+    public function getPolicyType()
+    {
+        return $this->container['policy_type'];
+    }
+
+    /**
+     * Sets policy_type
+     *
+     * @param string $policy_type 策略类型
+     *
+     * @return $this
+     */
+    public function setPolicyType($policy_type)
+    {
+        $this->container['policy_type'] = $policy_type;
+
+        return $this;
+    }
+
+    /**
+     * Gets surplus_duration_in_second
+     *
+     * @return int
+     */
+    public function getSurplusDurationInSecond()
+    {
+        return $this->container['surplus_duration_in_second'];
+    }
+
+    /**
+     * Sets surplus_duration_in_second
+     *
+     * @param int $surplus_duration_in_second 剩余试玩时长
+     *
+     * @return $this
+     */
+    public function setSurplusDurationInSecond($surplus_duration_in_second)
+    {
+        $this->container['surplus_duration_in_second'] = $surplus_duration_in_second;
+
+        return $this;
+    }
+    /**
+     * Returns true if offset exists. False otherwise.
+     *
+     * @param integer $offset Offset
+     *
+     * @return boolean
+     */
+    public function offsetExists($offset)
+    {
+        return isset($this->container[$offset]);
+    }
+
+    /**
+     * Gets offset.
+     *
+     * @param integer $offset Offset
+     *
+     * @return mixed
+     */
+    public function offsetGet($offset)
+    {
+        return isset($this->container[$offset]) ? $this->container[$offset] : null;
+    }
+
+    /**
+     * Sets value based on offset.
+     *
+     * @param integer $offset Offset
+     * @param mixed   $value  Value to be set
+     *
+     * @return void
+     */
+    public function offsetSet($offset, $value)
+    {
+        if (is_null($offset)) {
+            $this->container[] = $value;
+        } else {
+            $this->container[$offset] = $value;
+        }
+    }
+
+    /**
+     * Unsets offset.
+     *
+     * @param integer $offset Offset
+     *
+     * @return void
+     */
+    public function offsetUnset($offset)
+    {
+        unset($this->container[$offset]);
+    }
+
+    /**
+     * Gets the string presentation of the object
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        //if (defined('JSON_PRETTY_PRINT')) { // use JSON pretty print
+        //    return json_encode(
+        //        ObjectSerializer::sanitizeForSerialization($this),
+        //        JSON_PRETTY_PRINT
+        //    );
+        //}
+
+        return json_encode(ObjectSerializer::sanitizeForSerialization($this));
+    }
+}
