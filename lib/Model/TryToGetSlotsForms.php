bc2c02095525eb2c0a86a74428de348c55264c3f
Iven <huanghanwen.hhw@alibaba-inc.com>
2024-06-04 00:14:37 +0800

    feat: update

---
+++ lib/Model/TryToGetSlotsForms.php
@@ -0,0 +1,317 @@
+<?php
+/**
+ * TryToGetSlotsForms
+ *
+ * PHP version 5
+ *
+ * @category Class
+ * @package  Yjopenapi\Client
+ */
+
+/** Package client
+ * 
+ * 
+ * YuanJing OpenAPI SDK for PHP
+ *
+ * 
+ *
+ */
+
+namespace Yjopenapi\Client\Yjopenapi\Client\Yjopenapi\Client\Yjopenapi\Client\Model;
+
+use \ArrayAccess;
+use \Yjopenapi\Client\Yjopenapi\Client\Yjopenapi\Client\Yjopenapi\Client\Api\ObjectSerializer;
+
+/**
+ * TryToGetSlotsForms Class Doc Comment
+ *
+ * @category Class
+ * @package  Yjopenapi\Client
+ */
+class TryToGetSlotsForms implements ModelInterface, ArrayAccess
+{
+    const DISCRIMINATOR = null;
+
+    /**
+      * The original name of the model.
+      *
+      * @var string
+      */
+    protected static $serialModelName = 'TryToGetSlotsForms';
+
+    /**
+      * Array of property to type mappings. Used for (de)serialization
+      *
+      * @var string[]
+      */
+    protected static $serialTypes = [
+        'app_key' => 'string',
+'parallel_schedule' => 'bool',
+'requests' => '\Yjopenapi\Client\Yjopenapi\Client\Yjopenapi\Client\Yjopenapi\Client\Model\TryToGetSlotsFormsRequests[]'    ];
+
+    /**
+      * Array of property to format mappings. Used for (de)serialization
+      *
+      * @var string[]
+      */
+    protected static $serialFormats = [
+        'app_key' => null,
+'parallel_schedule' => null,
+'requests' => null    ];
+
+    /**
+     * Array of property to type mappings. Used for (de)serialization
+     *
+     * @return array
+     */
+    public static function serialTypes()
+    {
+        return self::$serialTypes;
+    }
+
+    /**
+     * Array of property to format mappings. Used for (de)serialization
+     *
+     * @return array
+     */
+    public static function serialFormats()
+    {
+        return self::$serialFormats;
+    }
+
+    /**
+     * Array of attributes where the key is the local name,
+     * and the value is the original name
+     *
+     * @var string[]
+     */
+    protected static $attributeMap = [
+        'app_key' => 'appKey',
+'parallel_schedule' => 'parallelSchedule',
+'requests' => 'requests'    ];
+
+    /**
+     * Array of attributes to setter functions (for deserialization of responses)
+     *
+     * @var string[]
+     */
+    protected static $setters = [
+        'app_key' => 'setAppKey',
+'parallel_schedule' => 'setParallelSchedule',
+'requests' => 'setRequests'    ];
+
+    /**
+     * Array of attributes to getter functions (for serialization of requests)
+     *
+     * @var string[]
+     */
+    protected static $getters = [
+        'app_key' => 'getAppKey',
+'parallel_schedule' => 'getParallelSchedule',
+'requests' => 'getRequests'    ];
+
+    /**
+     * Array of attributes where the key is the local name,
+     * and the value is the original name
+     *
+     * @return array
+     */
+    public static function attributeMap()
+    {
+        return self::$attributeMap;
+    }
+
+    /**
+     * Array of attributes to setter functions (for deserialization of responses)
+     *
+     * @return array
+     */
+    public static function setters()
+    {
+        return self::$setters;
+    }
+
+    /**
+     * Array of attributes to getter functions (for serialization of requests)
+     *
+     * @return array
+     */
+    public static function getters()
+    {
+        return self::$getters;
+    }
+
+    /**
+     * The original name of the model.
+     *
+     * @return string
+     */
+    public function getModelName()
+    {
+        return self::$serialModelName;
+    }
+
+    
+
+    /**
+     * Associative array for storing property values
+     *
+     * @var mixed[]
+     */
+    protected $container = [];
+
+    /**
+     * Constructor
+     *
+     * @param mixed[] $data Associated array of property values
+     *                      initializing the model
+     */
+    public function __construct(array $data = null)
+    {
+        $this->container['app_key'] = isset($data['app_key']) ? $data['app_key'] : null;
+        $this->container['parallel_schedule'] = isset($data['parallel_schedule']) ? $data['parallel_schedule'] : null;
+        $this->container['requests'] = isset($data['requests']) ? $data['requests'] : null;
+    }
+
+
+    /**
+     * Gets app_key
+     *
+     * @return string
+     */
+    public function getAppKey()
+    {
+        return $this->container['app_key'];
+    }
+
+    /**
+     * Sets app_key
+     *
+     * @param string $app_key app_key
+     *
+     * @return $this
+     */
+    public function setAppKey($app_key)
+    {
+        $this->container['app_key'] = $app_key;
+
+        return $this;
+    }
+
+    /**
+     * Gets parallel_schedule
+     *
+     * @return bool
+     */
+    public function getParallelSchedule()
+    {
+        return $this->container['parallel_schedule'];
+    }
+
+    /**
+     * Sets parallel_schedule
+     *
+     * @param bool $parallel_schedule parallel_schedule
+     *
+     * @return $this
+     */
+    public function setParallelSchedule($parallel_schedule)
+    {
+        $this->container['parallel_schedule'] = $parallel_schedule;
+
+        return $this;
+    }
+
+    /**
+     * Gets requests
+     *
+     * @return \Yjopenapi\Client\Yjopenapi\Client\Yjopenapi\Client\Yjopenapi\Client\Model\TryToGetSlotsFormsRequests[]
+     */
+    public function getRequests()
+    {
+        return $this->container['requests'];
+    }
+
+    /**
+     * Sets requests
+     *
+     * @param \Yjopenapi\Client\Yjopenapi\Client\Yjopenapi\Client\Yjopenapi\Client\Model\TryToGetSlotsFormsRequests[] $requests requests
+     *
+     * @return $this
+     */
+    public function setRequests($requests)
+    {
+        $this->container['requests'] = $requests;
+
+        return $this;
+    }
+    /**
+     * Returns true if offset exists. False otherwise.
+     *
+     * @param integer $offset Offset
+     *
+     * @return boolean
+     */
+    public function offsetExists($offset)
+    {
+        return isset($this->container[$offset]);
+    }
+
+    /**
+     * Gets offset.
+     *
+     * @param integer $offset Offset
+     *
+     * @return mixed
+     */
+    public function offsetGet($offset)
+    {
+        return isset($this->container[$offset]) ? $this->container[$offset] : null;
+    }
+
+    /**
+     * Sets value based on offset.
+     *
+     * @param integer $offset Offset
+     * @param mixed   $value  Value to be set
+     *
+     * @return void
+     */
+    public function offsetSet($offset, $value)
+    {
+        if (is_null($offset)) {
+            $this->container[] = $value;
+        } else {
+            $this->container[$offset] = $value;
+        }
+    }
+
+    /**
+     * Unsets offset.
+     *
+     * @param integer $offset Offset
+     *
+     * @return void
+     */
+    public function offsetUnset($offset)
+    {
+        unset($this->container[$offset]);
+    }
+
+    /**
+     * Gets the string presentation of the object
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        //if (defined('JSON_PRETTY_PRINT')) { // use JSON pretty print
+        //    return json_encode(
+        //        ObjectSerializer::sanitizeForSerialization($this),
+        //        JSON_PRETTY_PRINT
+        //    );
+        //}
+
+        return json_encode(ObjectSerializer::sanitizeForSerialization($this));
+    }
+}
