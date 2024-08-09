dcd35c0f49d20d70b940e4ef72d3522766c95561
Iven <huanghanwen.hhw@alibaba-inc.com>
2024-08-10 00:11:22 +0800

    feat: update

---
+++ lib/Model/ConsoleAdminGetGameInstanceContainerRatioResultModel.php
@@ -0,0 +1,317 @@
+<?php
+/**
+ * ConsoleAdminGetGameInstanceContainerRatioResultModel
+ *
+ * PHP version 5
+ *
+ * @category Class
+ * @package  Yjopenapi\Client
+ */
+
+/** Package client
+ * 
+ * 
+ * YuanJing OpenAPI SDK for PHP
+ *
+ * 
+ *
+ */
+
+namespace Yjopenapi\Client\Yjopenapi\Client\Yjopenapi\Client\Yjopenapi\Client\Yjopenapi\Client\Yjopenapi\Client\Model;
+
+use \ArrayAccess;
+use \Yjopenapi\Client\Yjopenapi\Client\Yjopenapi\Client\Yjopenapi\Client\Yjopenapi\Client\Yjopenapi\Client\Api\ObjectSerializer;
+
+/**
+ * ConsoleAdminGetGameInstanceContainerRatioResultModel Class Doc Comment
+ *
+ * @category Class
+ * @package  Yjopenapi\Client
+ */
+class ConsoleAdminGetGameInstanceContainerRatioResultModel implements ModelInterface, ArrayAccess
+{
+    const DISCRIMINATOR = null;
+
+    /**
+      * The original name of the model.
+      *
+      * @var string
+      */
+    protected static $serialModelName = 'ConsoleAdminGetGameInstanceContainerRatioResultModel';
+
+    /**
+      * Array of property to type mappings. Used for (de)serialization
+      *
+      * @var string[]
+      */
+    protected static $serialTypes = [
+        'mix_id' => 'string',
+'mix_version_id' => 'string',
+'container_ratio' => 'string'    ];
+
+    /**
+      * Array of property to format mappings. Used for (de)serialization
+      *
+      * @var string[]
+      */
+    protected static $serialFormats = [
+        'mix_id' => null,
+'mix_version_id' => null,
+'container_ratio' => null    ];
+
+    /**
+     * Array of property to type mappings. Used for (de)serialization
+     *
+     * @return array
+     */
+    public static function serialTypes()
+    {
+        return self::$serialTypes;
+    }
+
+    /**
+     * Array of property to format mappings. Used for (de)serialization
+     *
+     * @return array
+     */
+    public static function serialFormats()
+    {
+        return self::$serialFormats;
+    }
+
+    /**
+     * Array of attributes where the key is the local name,
+     * and the value is the original name
+     *
+     * @var string[]
+     */
+    protected static $attributeMap = [
+        'mix_id' => 'mixId',
+'mix_version_id' => 'mixVersionId',
+'container_ratio' => 'containerRatio'    ];
+
+    /**
+     * Array of attributes to setter functions (for deserialization of responses)
+     *
+     * @var string[]
+     */
+    protected static $setters = [
+        'mix_id' => 'setMixId',
+'mix_version_id' => 'setMixVersionId',
+'container_ratio' => 'setContainerRatio'    ];
+
+    /**
+     * Array of attributes to getter functions (for serialization of requests)
+     *
+     * @var string[]
+     */
+    protected static $getters = [
+        'mix_id' => 'getMixId',
+'mix_version_id' => 'getMixVersionId',
+'container_ratio' => 'getContainerRatio'    ];
+
+    /**
+     * Array of attributes where the key is the local name,
+     * and the value is the original name
+     *
+     * @return array
+     */
+    public static function attributeMap()
+    {
+        return self::$attributeMap;
+    }
+
+    /**
+     * Array of attributes to setter functions (for deserialization of responses)
+     *
+     * @return array
+     */
+    public static function setters()
+    {
+        return self::$setters;
+    }
+
+    /**
+     * Array of attributes to getter functions (for serialization of requests)
+     *
+     * @return array
+     */
+    public static function getters()
+    {
+        return self::$getters;
+    }
+
+    /**
+     * The original name of the model.
+     *
+     * @return string
+     */
+    public function getModelName()
+    {
+        return self::$serialModelName;
+    }
+
+    
+
+    /**
+     * Associative array for storing property values
+     *
+     * @var mixed[]
+     */
+    protected $container = [];
+
+    /**
+     * Constructor
+     *
+     * @param mixed[] $data Associated array of property values
+     *                      initializing the model
+     */
+    public function __construct(array $data = null)
+    {
+        $this->container['mix_id'] = isset($data['mix_id']) ? $data['mix_id'] : null;
+        $this->container['mix_version_id'] = isset($data['mix_version_id']) ? $data['mix_version_id'] : null;
+        $this->container['container_ratio'] = isset($data['container_ratio']) ? $data['container_ratio'] : null;
+    }
+
+
+    /**
+     * Gets mix_id
+     *
+     * @return string
+     */
+    public function getMixId()
+    {
+        return $this->container['mix_id'];
+    }
+
+    /**
+     * Sets mix_id
+     *
+     * @param string $mix_id mix_id
+     *
+     * @return $this
+     */
+    public function setMixId($mix_id)
+    {
+        $this->container['mix_id'] = $mix_id;
+
+        return $this;
+    }
+
+    /**
+     * Gets mix_version_id
+     *
+     * @return string
+     */
+    public function getMixVersionId()
+    {
+        return $this->container['mix_version_id'];
+    }
+
+    /**
+     * Sets mix_version_id
+     *
+     * @param string $mix_version_id mix_version_id
+     *
+     * @return $this
+     */
+    public function setMixVersionId($mix_version_id)
+    {
+        $this->container['mix_version_id'] = $mix_version_id;
+
+        return $this;
+    }
+
+    /**
+     * Gets container_ratio
+     *
+     * @return string
+     */
+    public function getContainerRatio()
+    {
+        return $this->container['container_ratio'];
+    }
+
+    /**
+     * Sets container_ratio
+     *
+     * @param string $container_ratio container_ratio
+     *
+     * @return $this
+     */
+    public function setContainerRatio($container_ratio)
+    {
+        $this->container['container_ratio'] = $container_ratio;
+
+        return $this;
+    }
+    /**
+     * Returns true if offset exists. False otherwise.
+     *
+     * @param integer $offset Offset
+     *
+     * @return boolean
+     */
+    public function offsetExists($offset)
+    {
+        return isset($this->container[$offset]);
+    }
+
+    /**
+     * Gets offset.
+     *
+     * @param integer $offset Offset
+     *
+     * @return mixed
+     */
+    public function offsetGet($offset)
+    {
+        return isset($this->container[$offset]) ? $this->container[$offset] : null;
+    }
+
+    /**
+     * Sets value based on offset.
+     *
+     * @param integer $offset Offset
+     * @param mixed   $value  Value to be set
+     *
+     * @return void
+     */
+    public function offsetSet($offset, $value)
+    {
+        if (is_null($offset)) {
+            $this->container[] = $value;
+        } else {
+            $this->container[$offset] = $value;
+        }
+    }
+
+    /**
+     * Unsets offset.
+     *
+     * @param integer $offset Offset
+     *
+     * @return void
+     */
+    public function offsetUnset($offset)
+    {
+        unset($this->container[$offset]);
+    }
+
+    /**
+     * Gets the string presentation of the object
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        //if (defined('JSON_PRETTY_PRINT')) { // use JSON pretty print
+        //    return json_encode(
+        //        ObjectSerializer::sanitizeForSerialization($this),
+        //        JSON_PRETTY_PRINT
+        //    );
+        //}
+
+        return json_encode(ObjectSerializer::sanitizeForSerialization($this));
+    }
+}
