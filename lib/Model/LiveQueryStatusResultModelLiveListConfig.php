070a711e5f5c5d99fb37d709ea93ebd5326f75d4
Iven <huanghanwen.hhw@alibaba-inc.com>
2024-09-30 00:14:10 +0800

    feat: update

---
+++ lib/Model/LiveQueryStatusResultModelLiveListConfig.php
@@ -0,0 +1,317 @@
+<?php
+/**
+ * LiveQueryStatusResultModelLiveListConfig
+ *
+ * PHP version 5
+ *
+ * @category Class
+ * @package  Yjopenapi\Client
+ */
+
+/** Package client
+ * 
+ * 
+ * YuanJing OpenAPI SDK for PHP
+ *
+ * 
+ *
+ */
+
+namespace Yjopenapi\Client\Model;
+
+use \ArrayAccess;
+use \Yjopenapi\Client\Api\ObjectSerializer;
+
+/**
+ * LiveQueryStatusResultModelLiveListConfig Class Doc Comment
+ *
+ * @category Class
+ * @package  Yjopenapi\Client
+ */
+class LiveQueryStatusResultModelLiveListConfig implements ModelInterface, ArrayAccess
+{
+    const DISCRIMINATOR = null;
+
+    /**
+      * The original name of the model.
+      *
+      * @var string
+      */
+    protected static $serialModelName = 'LiveQueryStatusResultModelLiveListConfig';
+
+    /**
+      * Array of property to type mappings. Used for (de)serialization
+      *
+      * @var string[]
+      */
+    protected static $serialTypes = [
+        'frame_rate' => 'int',
+'bitrate' => 'int',
+'resolution' => 'string'    ];
+
+    /**
+      * Array of property to format mappings. Used for (de)serialization
+      *
+      * @var string[]
+      */
+    protected static $serialFormats = [
+        'frame_rate' => null,
+'bitrate' => null,
+'resolution' => null    ];
+
+    /**
+     * Array of property to type mappings. Used for (de)serialization
+     *
+     * @return array
+     */
+    public static function serialTypes()
+    {
+        return self::$serialTypes;
+    }
+
+    /**
+     * Array of property to format mappings. Used for (de)serialization
+     *
+     * @return array
+     */
+    public static function serialFormats()
+    {
+        return self::$serialFormats;
+    }
+
+    /**
+     * Array of attributes where the key is the local name,
+     * and the value is the original name
+     *
+     * @var string[]
+     */
+    protected static $attributeMap = [
+        'frame_rate' => 'frameRate',
+'bitrate' => 'bitrate',
+'resolution' => 'resolution'    ];
+
+    /**
+     * Array of attributes to setter functions (for deserialization of responses)
+     *
+     * @var string[]
+     */
+    protected static $setters = [
+        'frame_rate' => 'setFrameRate',
+'bitrate' => 'setBitrate',
+'resolution' => 'setResolution'    ];
+
+    /**
+     * Array of attributes to getter functions (for serialization of requests)
+     *
+     * @var string[]
+     */
+    protected static $getters = [
+        'frame_rate' => 'getFrameRate',
+'bitrate' => 'getBitrate',
+'resolution' => 'getResolution'    ];
+
+    /**
+     * Array of attributes where the key is the local name,
+     * and the value is the original name
+     *
+     * @return array
+     */
+    public static function attributeMap()
+    {
+        return self::$attributeMap;
+    }
+
+    /**
+     * Array of attributes to setter functions (for deserialization of responses)
+     *
+     * @return array
+     */
+    public static function setters()
+    {
+        return self::$setters;
+    }
+
+    /**
+     * Array of attributes to getter functions (for serialization of requests)
+     *
+     * @return array
+     */
+    public static function getters()
+    {
+        return self::$getters;
+    }
+
+    /**
+     * The original name of the model.
+     *
+     * @return string
+     */
+    public function getModelName()
+    {
+        return self::$serialModelName;
+    }
+
+    
+
+    /**
+     * Associative array for storing property values
+     *
+     * @var mixed[]
+     */
+    protected $container = [];
+
+    /**
+     * Constructor
+     *
+     * @param mixed[] $data Associated array of property values
+     *                      initializing the model
+     */
+    public function __construct(array $data = null)
+    {
+        $this->container['frame_rate'] = isset($data['frame_rate']) ? $data['frame_rate'] : null;
+        $this->container['bitrate'] = isset($data['bitrate']) ? $data['bitrate'] : null;
+        $this->container['resolution'] = isset($data['resolution']) ? $data['resolution'] : null;
+    }
+
+
+    /**
+     * Gets frame_rate
+     *
+     * @return int
+     */
+    public function getFrameRate()
+    {
+        return $this->container['frame_rate'];
+    }
+
+    /**
+     * Sets frame_rate
+     *
+     * @param int $frame_rate frame_rate
+     *
+     * @return $this
+     */
+    public function setFrameRate($frame_rate)
+    {
+        $this->container['frame_rate'] = $frame_rate;
+
+        return $this;
+    }
+
+    /**
+     * Gets bitrate
+     *
+     * @return int
+     */
+    public function getBitrate()
+    {
+        return $this->container['bitrate'];
+    }
+
+    /**
+     * Sets bitrate
+     *
+     * @param int $bitrate bitrate
+     *
+     * @return $this
+     */
+    public function setBitrate($bitrate)
+    {
+        $this->container['bitrate'] = $bitrate;
+
+        return $this;
+    }
+
+    /**
+     * Gets resolution
+     *
+     * @return string
+     */
+    public function getResolution()
+    {
+        return $this->container['resolution'];
+    }
+
+    /**
+     * Sets resolution
+     *
+     * @param string $resolution resolution
+     *
+     * @return $this
+     */
+    public function setResolution($resolution)
+    {
+        $this->container['resolution'] = $resolution;
+
+        return $this;
+    }
+    /**
+     * Returns true if offset exists. False otherwise.
+     *
+     * @param integer $offset Offset
+     *
+     * @return boolean
+     */
+    public function offsetExists($offset)
+    {
+        return isset($this->container[$offset]);
+    }
+
+    /**
+     * Gets offset.
+     *
+     * @param integer $offset Offset
+     *
+     * @return mixed
+     */
+    public function offsetGet($offset)
+    {
+        return isset($this->container[$offset]) ? $this->container[$offset] : null;
+    }
+
+    /**
+     * Sets value based on offset.
+     *
+     * @param integer $offset Offset
+     * @param mixed   $value  Value to be set
+     *
+     * @return void
+     */
+    public function offsetSet($offset, $value)
+    {
+        if (is_null($offset)) {
+            $this->container[] = $value;
+        } else {
+            $this->container[$offset] = $value;
+        }
+    }
+
+    /**
+     * Unsets offset.
+     *
+     * @param integer $offset Offset
+     *
+     * @return void
+     */
+    public function offsetUnset($offset)
+    {
+        unset($this->container[$offset]);
+    }
+
+    /**
+     * Gets the string presentation of the object
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        //if (defined('JSON_PRETTY_PRINT')) { // use JSON pretty print
+        //    return json_encode(
+        //        ObjectSerializer::sanitizeForSerialization($this),
+        //        JSON_PRETTY_PRINT
+        //    );
+        //}
+
+        return json_encode(ObjectSerializer::sanitizeForSerialization($this));
+    }
+}
