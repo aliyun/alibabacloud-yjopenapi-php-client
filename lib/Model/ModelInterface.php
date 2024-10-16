16d5bdd21cffdb79a31a23d5decbbbb8ec68db42
Iven <huanghanwen.hhw@alibaba-inc.com>
2024-10-17 00:16:53 +0800

    feat: update

---
+++ lib/Model/ModelInterface.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * ModelInterface
+ *
+ * PHP version 5
+ *
+ * @category Class
+ * @package  Yjopenapi\Client\Yjopenapi\Client\Yjopenapi\Client\Model
+ */
+
+/** Package client
+ * 
+ * 
+ * YuanJing OpenAPI SDK for PHP
+ *
+ * 
+ *
+ */
+
+namespace Yjopenapi\Client\Yjopenapi\Client\Yjopenapi\Client\Model;
+
+/**
+ * Interface abstracting model access.
+ *
+ * @package Yjopenapi\Client\Yjopenapi\Client\Yjopenapi\Client\Model
+ */
+interface ModelInterface
+{
+    const Trace_Id = 'Traceid';
+    const Result_Status = 'Result-Status';
+
+    /**
+     * The original name of the model.
+     *
+     * @return string
+     */
+    public function getModelName();
+
+    /**
+     * Array of property to type mappings. Used for (de)serialization
+     *
+     * @return array
+     */
+    public static function serialTypes();
+
+    /**
+     * Array of property to format mappings. Used for (de)serialization
+     *
+     * @return array
+     */
+    public static function serialFormats();
+
+    /**
+     * Array of attributes where the key is the local name, and the value is the original name
+     *
+     * @return array
+     */
+    public static function attributeMap();
+
+    /**
+     * Array of attributes to setter functions (for deserialization of responses)
+     *
+     * @return array
+     */
+    public static function setters();
+
+    /**
+     * Array of attributes to getter functions (for serialization of requests)
+     *
+     * @return array
+     */
+    public static function getters();
+
+}
